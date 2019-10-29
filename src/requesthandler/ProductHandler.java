e033755bd5db429ea42fc3536125b5a59fb4b671
Zeyad Yasser <zeyady98@gmail.com>
2019-10-29 20:58:55 +0200

    fix bug in ProductService

--- src/requesthandler/ProductHandler.java
+++ src/requesthandler/ProductHandler.java
@@ -14,11 +14,9 @@ public class ProductHandler implements IRequestHandler {
     @Override
     public void handle(IContext ctx) throws IOException {
         ctx.parse();
-        System.out.println(ctx.getParam(""));
         IProduct product = new Product(ctx.getParam("m_name"), ctx.getParam("m_brand"),
                 Double.valueOf(ctx.getParam("m_price")), ctx.getParam("m_category"));
         m_service.addProduct(product);
         ctx.write("<html>Product Added</html>".getBytes());
-
     }
 }
