4fab9789a33be98a4b18d04baa9da8e5561002f6
Zeyad Yasser <zeyady98@gmail.com>
2019-11-01 18:09:42 +0200

    allow Admin only to view addProductPage

--- src/requesthandler/AddProductPageHandler.java
+++ src/requesthandler/AddProductPageHandler.java
@@ -1,14 +1,21 @@
 package requesthandler;
 
 import context.IContext;
+import model.account.AccountType;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class AddProductPageHandler implements IRequestHandler {
+public class AddProductPageHandler extends RequestHandlerBase {
     @Override
     public void handle(IContext ctx) throws IOException {
+    	if(ctx.getUser() == null || ctx.getUser().getType() != AccountType.ADMIN) {
+    		ctx.write("<h1>unauthorized</h1>".getBytes());
+    		return;
+    	}
+    		
+
         String output = "";
         File homeFile = new File("src/product.html");
         Scanner scanner = new Scanner(homeFile);
