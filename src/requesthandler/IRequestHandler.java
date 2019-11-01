9207a064eb2bacf08773bd3cff7e4ac86d2a0191
Zeyad Yasser <zeyady98@gmail.com>
2019-11-01 18:12:10 +0200

    add SessionManager setter and getter

--- src/requesthandler/IRequestHandler.java
+++ src/requesthandler/IRequestHandler.java
@@ -3,8 +3,11 @@ package requesthandler;
 import java.io.IOException;
 
 import context.IContext;
+import session.ISessionManager;
 
 public interface IRequestHandler {
 	public void handle(IContext ctx) throws IOException;
+	public void setSessionManager(ISessionManager smgr);
+	public ISessionManager getSessionManager();
 	//TODO htmlreader function public String htmlRead(String path);
 }
