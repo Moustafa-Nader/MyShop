263e0153d0efe9f97edd99ad050c68383505e13b
Zeyad Yasser <zeyady98@gmail.com>
2019-11-01 18:03:08 +0200

    add Base for Request Handler for common tasks

---
+++ src/requesthandler/RequestHandlerBase.java
@@ -0,0 +1,25 @@
+package requesthandler;
+
+import java.io.IOException;
+
+import context.IContext;
+import session.ISessionManager;
+
+public class RequestHandlerBase implements IRequestHandler {
+	ISessionManager m_smgr;
+	
+	@Override
+	public void handle(IContext ctx) throws IOException {
+		// no implementation
+	}
+
+	@Override
+	public void setSessionManager(ISessionManager smgr) {
+		m_smgr = smgr;
+	}
+	
+	@Override
+	public ISessionManager getSessionManager() {
+		return m_smgr;
+	}
+}
