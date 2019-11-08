410fe3979766c10ab9a1cbb8a8d1bf49dbd9c9c5
Zeyad Yasser <zeyady98@gmail.com>
2019-11-08 13:37:49 +0200

    detect if user is logged in home page

--- src/requesthandler/HomeHandler.java
+++ src/requesthandler/HomeHandler.java
@@ -9,12 +9,27 @@ import java.util.Scanner;
 public class HomeHandler extends RequestHandlerBase {
     @Override
     public void handle(IContext ctx) throws IOException {
+    	if(ctx.getUser() == null) {
+    		this.showLogin(ctx);
+    		return;
+    	} else {
+    		this.showHome(ctx);
+    	}
+    }
+    
+    private void showLogin(IContext ctx) throws IOException {
         String output = "";
-        File homeFile = new File("src/home.html");
+        File homeFile = new File("src/login_form.html");
         Scanner scanner = new Scanner(homeFile);
         while(scanner.hasNextLine())
             output += scanner.nextLine();
 
         ctx.write(output.getBytes());
+    }
+    
+    private void showHome(IContext ctx) throws IOException {
+    	String username = ctx.getUser().getUsername();
+    	String htmlData = "<h1> welcome back " + username + "!</h1>";
+    	ctx.write(htmlData.getBytes());
     }
 }
