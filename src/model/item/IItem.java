f8f4d806e19bb25d899a4cdf0866e05823d8cdd2
Zeyad Yasser <zeyady98@gmail.com>
2019-11-08 14:36:44 +0200

    add Item interface & class

---
+++ src/model/item/IItem.java
@@ -0,0 +1,12 @@
+package model.item;
+
+public interface IItem {
+	public void setID(int id);
+	public void setProductID(int productId);
+	public void setStoreID(int storeId);
+	public void setPrice(double price);
+	public int getID();
+	public int getProductID();
+	public int getStoreID();
+	public double getPrice();
+}
