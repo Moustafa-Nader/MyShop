06d5bb7d47163d67398a6065ae7c941fd00322d5
Zeyad Yasser <zeyady98@gmail.com>
2019-11-28 02:10:03 +0200

    add Order & OrderService

---
+++ src/model/order/Order.java
@@ -0,0 +1,50 @@
+package model.order;
+
+import java.util.Date;
+
+public class Order implements IOrder {
+	int m_orderId, m_itemId, m_userId;
+	Date m_date;
+	
+	public Order(int orderId, int itemId, int userId) {
+		m_date = new Date();
+		m_orderId = orderId;
+		m_itemId = itemId;
+		m_userId = userId;
+	}
+	
+	@Override
+	public void setID(int orderId) {
+		m_orderId = orderId;
+	}
+
+	@Override
+	public void setItemID(int itemId) {
+		m_itemId = itemId;
+	}
+
+	@Override
+	public void setUserID(int userId) {
+		m_userId = userId;
+	}
+
+	@Override
+	public int getID() {
+		return m_orderId;
+	}
+
+	@Override
+	public int getItemID() {
+		return m_itemId;
+	}
+
+	@Override
+	public int getUserID() {
+		return m_userId;
+	}
+
+	@Override
+	public Date getDate() {
+		return m_date;
+	}
+}
