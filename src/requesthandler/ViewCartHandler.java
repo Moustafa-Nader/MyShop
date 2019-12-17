8f77e60cd911d272eccf93237a22e50b9696c568
OVGB <lan12131999@gmail.com>
2019-12-17 13:14:37 +0200

    edited ViewCartHandler

--- src/requesthandler/ViewCartHandler.java
+++ src/requesthandler/ViewCartHandler.java
@@ -3,21 +3,25 @@ package requesthandler;
 import java.io.IOException;
 
 import context.IContext;
+import model.Store.IStore;
 import model.cart.Cart;
 import model.cart.CartItem;
 import model.cart.ICart;
 import model.item.IItem;
 import model.product.IProduct;
+import service.order.IOrderService;
 import service.product.IProductService;
 import service.statistics.IStatisticsService;
 import service.store.IStoreService;
 
 public class ViewCartHandler extends RequestHandlerBase {
     private IProductService m_productService;
-    public ViewCartHandler(IProductService productService){
-
+    private IOrderService m_orderService;
+    private IStoreService m_storeService;
+    public ViewCartHandler(IProductService productService, IOrderService orderService, IStoreService storeService){
+        this.m_orderService = orderService;
         this.m_productService = productService;
-
+        this.m_storeService = storeService;
     }
 
 	public void handle(IContext ctx) throws IOException {
@@ -29,6 +33,7 @@ public class ViewCartHandler extends RequestHandlerBase {
         if(useCart.getCartItems().isEmpty()) {
             ctx.write("<html><h1>Empty Cart!</h1></html>".getBytes());
         }
+        int userOrderCount = m_orderService.getOrdersCountByUserId(ctx.getUser().getID());
         output += "<!DOCTYPE html>\n" +
 					"<html>\n" +
 					"<head>\n" +
@@ -38,9 +43,12 @@ public class ViewCartHandler extends RequestHandlerBase {
                     "<body>\n" +
                     "\t<form  action=\"/makepurchase/" + "\"method=\"get\">\n" ;
                 for(CartItem cartItem : useCart.getCartItems()){
+                    System.out.println("Cart Size:" + useCart.getCartItems().size());
+                    // product = m_productService.getProductByID(cartItem.getItem_id());
                     output += "\t<h1 >" +
                     m_productService.getProductByID(cartItem.getItem_id()).getName() +" "+ cartItem.getQuantity()+
-                    "</h1>\n";   
+                            //" " + m_storeService.getItemByID(cartItem.getItem_id()).getPrice() +
+                    "</h1>\n<b>";
                 }
                 output+="\t\t<div class=\"form-box\">\n" +
                 "\t\t\t<button id=\"makepurchase\" type=\"submit\">Purchase</button>\n" +
