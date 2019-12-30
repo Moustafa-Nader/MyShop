ce608879ff0695f2d8deecc492e2cfb51bb85c16
Zeyad Yasser <zeyady98@gmail.com>
2019-12-30 15:24:29 +0200

    add comments to brand service

--- src/service/brand/BrandService.java
+++ src/service/brand/BrandService.java
@@ -7,21 +7,37 @@ import service.IAggregate;
 
 public class BrandService implements IBrandService, IAggregate {
     ArrayList<IBrand> m_brands;
-    
-    public ArrayList<IBrand> getBrandList(){
-        return this.m_brands;
-    }
+ 
 
     public BrandService() {
         this.m_brands = new ArrayList<>();
     }
-
+    
+    /**
+     * Returns a list of the available brands
+     */
+    public ArrayList<IBrand> getBrandList(){
+        return this.m_brands;
+    }
+    
+    
+    /**
+     * Add brand to data store
+     * 
+     * @param brand 	instance of the brand to add
+     */
     public void addBrand(IBrand brand) {
         brand.setID(m_brands.size());
         m_brands.add(brand);
         
     }
 
+    
+    /**
+     * Retrieves brand object by its name
+     * 
+     * @param Name		brand name to get
+     */
     public IBrand getBrandByName(String Name) {
         for (int i = 0; i < m_brands.size(); i++)
             if (m_brands.get(i).getName().equals(Name))
@@ -29,6 +45,12 @@ public class BrandService implements IBrandService, IAggregate {
         return null;
 
     }
+    
+    /**
+     * Checks brand exists its name
+     * 
+     * @param Name		brand name to check
+     */
     public boolean Contains(String Name){
         IBrand brand = getBrandByName(Name);
         if(brand != null) return true;
@@ -36,6 +58,10 @@ public class BrandService implements IBrandService, IAggregate {
         
     }
 
+    
+	/**
+	 *	Returns number of brands in the data store
+	 */
 	@Override
 	public int count() {
 		return m_brands.size();
